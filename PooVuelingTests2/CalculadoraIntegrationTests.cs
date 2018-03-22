33b5ebf290e2b06854435d9fecdc861a39e8815c
danigracia <gracia.danii@gmail.com>
2018-03-22 13:26:02 +0100

    all test

---
+++ PooVuelingTests2/CalculadoraIntegrationTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PooVueling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooVueling.Tests
+{
+    [TestClass()]
+    public class CalculadoraIntegrationTests
+    {
+        ICalculadora iCalculadora = new Calculadora();
+
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            Assert.IsTrue(iCalculadora.Division(4, 2) == 2);
+        }
+
+        [TestMethod()]
+        public void MultiplicacionTest()
+        {
+            Assert.IsTrue(iCalculadora.Multiplicacion(2, 2) == 4);
+        }
+
+        [TestMethod()]
+        public void RestaTest()
+        {
+            Assert.IsTrue(iCalculadora.Resta(4, 2) == 2);
+        }
+
+        [TestMethod()]
+        public void SumaTest()
+        {
+            Assert.IsTrue(iCalculadora.Suma(2, 2) == 4);
+        }
+    }
+}
---
+++ PooVuelingXunitTest/CalculadoraIntergationXunitTest.cs
@@ -0,0 +1,35 @@
+﻿using PooVueling;
+using Xunit;
+
+namespace PooVuelingXunitTest
+{
+    public class CalculadoraIntegrationXunitTests
+    {
+        ICalculadora iCalculadora = new Calculadora();
+
+        [Fact]
+        public void SumaTest()
+        {
+            Assert.Equal(4, iCalculadora.Suma(1, 3));
+        }
+
+        [Fact]
+        public void RestaTest()
+        {
+            Assert.NotEqual(5, iCalculadora.Resta(7, 3));
+        }
+
+        [Fact]
+        public void DivisionTest()
+        {
+            Assert.False(5 == iCalculadora.Division(32, 8));
+        }
+
+        [Fact]
+        public void MultiplicacionTest()
+        {
+            Assert.True(20 == iCalculadora.Multiplicacion(4, 5));
+        }
+
+    }
+}
